18b47005c6987fcee439117e8d3a9140e3fe5404
darekz <dziembinski@gmail.com>
2018-03-01 00:06:04 -0600

    Basic routing

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -14,11 +14,25 @@ namespace Vidly.Controllers
 
         {
             var movie = new Movie() { Name="Shrek!"};
-            //return  View(movie);
+            return  View(movie);
             //return Content("Darekz");
             //return HttpNotFound();
             //return new EmptyResult();
-            return RedirectToAction("Index", "Home", new { page = 1, sortBy = "name" });
+            //return RedirectToAction("Index", "Home", new { page = 1, sortBy = "name" });
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return Content("id=" + id);
+        }
+
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+                pageIndex = 1;
+            if (string.IsNullOrWhiteSpace(sortBy))
+                sortBy = "Name";
+            return Content(string.Format("pageIndex={0}&sortBy={1}",pageIndex,sortBy));
         }
     }
 }
